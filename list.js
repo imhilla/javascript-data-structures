62a7a0dd3481c3c89f2835321bf97d5e7b278724
imhilla <hillaryodhiambo282@gmail.com>
2020-08-09 11:58:37 +0300

    add traverse functions

--- list.js
+++ list.js
@@ -8,17 +8,18 @@ function List() {
   this.insert = insert;
   this.append = append;
   this.remove = remove;
-  //this.front = front;
-  // this.end = end;
-  // this.prev = prev;
-  // this.next = next;
-  // this.length = length;
-  // this.currPos = currPos;
-  // this.moveTo = moveTo;
-  // this.getElement = getElement;
-  // this.length = length;
-  // this.contains = contains;
+  this.front = front;
+  this.end = end;
+  this.prev = prev;
+  this.next = next;
+  this.length = length;
+  this.currPos = currPos;
+  this.moveTo = moveTo;
+  this.getElement = getElement;
+  this.length = length;
+  this.contains = contains;
 }
+
 //Adding an Element to a List
 
 function append(element) {
@@ -92,4 +93,39 @@ function contains(element) {
     }
   }
   return false;
+}
+
+//Traversing a List
+//This final set of functions allows movement through a list, and the last function,
+//getElement() , displays the current element in a list:
+function front() {
+  this.pos = 0;
+}
+
+function end() {
+  this.pos = this.listSize - 1;
+}
+
+function prev() {
+  if (this.pos > 0) {
+    --this.pos;
+  }
+}
+
+function next() {
+  if (this.pos < this.listSize - 1) {
+    ++this.pos;
+  }
+}
+
+function currPos() {
+  return this.pos;
+}
+
+function moveTo(position) {
+  this.pos = position;
+}
+
+function getElement() {
+  return this.dataStore[this.pos];
 }
