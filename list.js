ccbca1ec623aa47acef2dbed45949031ae9390f0
imhilla <hillaryodhiambo282@gmail.com>
2020-08-09 11:37:21 +0300

    modified list.js

--- list.js
+++ list.js
@@ -2,22 +2,22 @@ function List() {
   this.listSize = 0;
   this.pos = 0;
   this.dataStore = []; // initializes an empty array to store list elements
-  this.clear = clear;
+  //this.clear = clear;
   this.find = find;
   this.toString = toString;
   this.insert = insert;
   this.append = append;
   this.remove = remove;
-  this.front = front;
-  this.end = end;
-  this.prev = prev;
-  this.next = next;
-  this.length = length;
-  this.currPos = currPos;
-  this.moveTo = moveTo;
-  this.getElement = getElement;
-  this.length = length;
-  this.contains = contains;
+  //this.front = front;
+  // this.end = end;
+  // this.prev = prev;
+  // this.next = next;
+  // this.length = length;
+  // this.currPos = currPos;
+  // this.moveTo = moveTo;
+  // this.getElement = getElement;
+  // this.length = length;
+  // this.contains = contains;
 }
 //Adding an Element to a List
 
@@ -61,3 +61,20 @@ function toString() {
 var names = new List();
 names.append("Cynthia");
 names.append("Raymond");
+names.append("Barbara");
+print(names.toString());
+names.remove("Raymond");
+print(names.toString());
+
+//Insert: Inserting an Element into a List
+function insert(element, after) {
+  var insertPos = this.find(after);
+  if (insertPos > -1) {
+    this.dataStore.splice(insertPos + 1, 0, element);
+    ++this.listSize;
+    return true;
+  }
+  return false;
+}
+
+//Clear: Removing All Elements from a List
